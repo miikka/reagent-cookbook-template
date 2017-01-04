ec8d28bb5d3a2a3fe8fc14822b2053e19ce9c67d
Miikka Koskinen <miikka.koskinen@iki.fi>
2017-01-04 19:59:08 +0200

    Bump deps in the generated project.clj

--- src/leiningen/new/rc/project.clj
+++ src/leiningen/new/rc/project.clj
@@ -1,11 +1,11 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
-                 [reagent "0.5.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [reagent "0.6.0"]]
 
   :source-paths ["src/clj"]
 
-  :plugins [[lein-cljsbuild "1.0.6"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target" "test/js"]
 
